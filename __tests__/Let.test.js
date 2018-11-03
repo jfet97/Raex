eb9079b5b1b35e0fc22277d4ada549733444f67f
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-11-03 22:24:09 +0100

    Update Let.test.js

--- __tests__/Let.test.js
+++ __tests__/Let.test.js
@@ -6,7 +6,7 @@ let a = new Let(1);
 let b = new Let(2);
 let c = new Let(3);
 
-let d = Let.add(a, b, c);
+let d = Let.add(a, b, c); // +
 console.log(d.value); // 6
 d.onChange((v) => console.log(`value updated: ${v}`));
 
@@ -20,11 +20,11 @@ console.log("-------------------------------------------");
 
 let s1 = new Let(100);
 let s2 = new Let(90);
-let sT = Let.sub(s1, s2);
-console.log(sT.value);
+let sT = Let.sub(s1, s2); // -
+console.log(sT.value); // 10
 
 s2.value = 73;
-console.log(sT.value);
+console.log(sT.value); // 27
 
 
 console.log("-------------------------------------------");
@@ -32,31 +32,31 @@ console.log("-------------------------------------------");
 let f1 = new Let(5);
 let f2 = new Let(2);
 
-let resM = Let.mult(f1, f2);
-console.log(resM.value);
+let resM = Let.mult(f1, f2); // *
+console.log(resM.value); // 10
 
 f2.value = 7;
-console.log(resM.value);
+console.log(resM.value); // 35
 
 
 console.log("-------------------------------------------");
 
 let d1 = new Let(10);
 let d2 = new Let(5);
-let resD = Let.div(d1, d2);
+let resD = Let.div(d1, d2); // /
 
-console.log(resD.value);
+console.log(resD.value); // 2
 d2.value = 2;
-console.log(resD.value);
+console.log(resD.value); // 5
 
 console.log("-------------------------------------------");
 
 let md1 = new Let(10);
 let md2 = new Let(3);
-let resMD = Let.mod(md1, md2);
+let resMD = Let.mod(md1, md2); // % 
 
-console.log(resMD.value);
+console.log(resMD.value); // 1
 md2.value = 4
-console.log(resMD.value);
+console.log(resMD.value); // 2
 
 console.log("-------------------------------------------");
