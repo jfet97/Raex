139720afc8077dc7e769dce245bb837ee5335670
Valentino Losito <valentino.losito@mhr.co.uk>
2018-11-07 18:36:49 +0000

    avoid usage of new, separated operations from main context

--- __tests__/Let.test.js
+++ __tests__/Let.test.js
@@ -1,12 +1,12 @@
-import { Let } from '../lib';
+import {Let, Operations} from '../lib';
 
 console.log("-------------------------------------------");
 
-let a = new Let(1);
-let b = new Let(2);
-let c = new Let(3);
+let a = Let(1);
+let b = Let(2);
+let c = Let(3);
 
-let d = Let.add(a, b, c); // +
+let d = Operations.add(a, b, c); // +
 console.log(d.value); // 6
 d.onChange((v) => console.log(`value updated: ${v}`));
 
@@ -18,32 +18,30 @@ console.log(d.value); // 10;
 
 console.log("-------------------------------------------");
 
-let s1 = new Let(100);
-let s2 = new Let(90);
-let sT = Let.sub(s1, s2); // -
+let s1 = Let(100);
+let s2 = Let(90);
+let sT = Operations.sub(s1, s2); // -
 console.log(sT.value); // 10
 
 s2.value = 73;
 console.log(sT.value); // 27
 
-
 console.log("-------------------------------------------");
 
-let f1 = new Let(5);
-let f2 = new Let(2);
+let f1 = Let(5);
+let f2 = Let(2);
 
-let resM = Let.mult(f1, f2); // *
+let resM = Operations.mult(f1, f2); // *
 console.log(resM.value); // 10
 
 f2.value = 7;
 console.log(resM.value); // 35
 
-
 console.log("-------------------------------------------");
 
-let d1 = new Let(10);
-let d2 = new Let(5);
-let resD = Let.div(d1, d2); // /
+let d1 = Let(10);
+let d2 = Let(5);
+let resD = Operations.div(d1, d2); // /
 
 console.log(resD.value); // 2
 d2.value = 2;
@@ -51,12 +49,12 @@ console.log(resD.value); // 5
 
 console.log("-------------------------------------------");
 
-let md1 = new Let(10);
-let md2 = new Let(3);
-let resMD = Let.mod(md1, md2); // % 
+let md1 = Let(10);
+let md2 = Let(3);
+let resMD = Operations.mod(md1, md2); // %
 
 console.log(resMD.value); // 1
-md2.value = 4
+md2.value = 4;
 console.log(resMD.value); // 2
 
 console.log("-------------------------------------------");
